31204c6b25ac1791f6e4305dd14e7b3b97f5a7e2
in1t5 <inithandshake@googlemail.com>
2017-01-19 21:34:50 +0100

    Adding test case for camel route

---
+++ src/test/java/route/MyCamelRouteTest.java
@@ -0,0 +1,45 @@
+package route;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.CamelSpringDelegatingTestContextLoader;
+import org.apache.camel.test.spring.CamelSpringRunner;
+import org.apache.camel.test.spring.MockEndpoints;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+
+@RunWith(CamelSpringRunner.class)
+@ContextConfiguration(classes = { AppConfig.class },
+		// Since Camel 2.11.0
+		loader = CamelSpringDelegatingTestContextLoader.class)
+@MockEndpoints
+public class MyCamelRouteTest {
+
+	@EndpointInject(uri = "mock:direct:end")
+	protected MockEndpoint endEndpoint;
+
+	@EndpointInject(uri = "mock:direct:error")
+	protected MockEndpoint errorEndpoint;
+
+	@Produce(uri = "direct:test")
+	protected ProducerTemplate testProducer;
+
+	@Test
+	public void testRoute() throws InterruptedException {
+		endEndpoint.expectedMessageCount(1);
+		errorEndpoint.expectedMessageCount(0);
+
+		testProducer.sendBody("<name>test</name>");
+
+		endEndpoint.assertIsSatisfied();
+		errorEndpoint.assertIsSatisfied();
+
+		assertThat(endEndpoint.getExchanges()).size().isEqualTo(1);
+
+	}
+}
